d395405805ab0613bc8e534317564addcdac5a68
Oksana Oberemok <Pylypenkok@gmail.com>
2021-08-30 18:50:20 +0300

    added description ,favicon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React from "react";
 import "./App.css";
 import Table from "./Table";
 
+function getBMIDescription(BMI) {
+  if (BMI < 16) {
+    return "Underweight (Severe thinness)";
+  } else if (BMI >= 16 && BMI < 16.9) {
+    return "Underweight (Moderate thinness)";
+  } else if (BMI > 17 && BMI < 18.4) {
+    return "Underweight (Mild thinness)";
+  } else if (BMI > 18.5 && BMI < 24.9) {
+    return "Normal range";
+  } else if (BMI > 25 && BMI < 29.9) {
+    return "Overweight (Pre-obese)";
+  } else if (BMI > 30 && BMI < 34.9) {
+    return "Obese (Class I)";
+  } else if (BMI > 35 && BMI < 39.9) {
+    return "Obese (Class II)";
+  } else {
+    return "Obese (Class III)";
+  }
+}
+
 function App() {
   const [weight, setWeight] = React.useState("");
   const [height, setHeight] = React.useState("");
@@ -18,6 +38,8 @@ function App() {
     const bmi =
       parseInt((weight / (heightInMeter * heightInMeter)) * 100) / 100;
     setBMI(bmi);
+    setHeight("");
+    setWeight("");
   }
 
   return (
@@ -39,7 +61,13 @@ function App() {
       <button onClick={countBMI} className="button">
         Count
       </button>
-      {BMI ? <p>Your BMI: {BMI} </p> : <p>Enter your data to count a BMI</p>}
+      {BMI ? (
+        <p>
+          Your BMI: <strong>{BMI}</strong>. {getBMIDescription(BMI)}{" "}
+        </p>
+      ) : (
+        <p>Enter your data to count a BMI</p>
+      )}
       <Table />
     </div>
   );
--- src/Table.js
+++ src/Table.js
@@ -3,42 +3,44 @@ import React from "react";
 function Table() {
   return (
     <table className="table">
-      <tr>
-        <th>Category</th>
-        <th>BMI (kg/m2)</th>
-      </tr>
-      <tr>
-        <td>Underweight (Severe thinness)</td>
-        <td> &lt; 16.0</td>
-      </tr>
-      <tr>
-        <td>Underweight (Moderate thinness)</td>
-        <td>16.0 – 16.9</td>
-      </tr>
-      <tr>
-        <td>Underweight (Mild thinness)</td>
-        <td>17.0 – 18.4</td>
-      </tr>
-      <tr>
-        <td>Normal range</td>
-        <td>18.5 – 24.9</td>
-      </tr>
-      <tr>
-        <td>Overweight (Pre-obese)</td>
-        <td>25.0 – 29.9</td>
-      </tr>
-      <tr>
-        <td>Obese (Class I)</td>
-        <td>30.0 – 34.9</td>
-      </tr>
-      <tr>
-        <td>Obese (Class II)</td>
-        <td>35.0 – 39.9</td>
-      </tr>
-      <tr>
-        <td>Obese (Class III)</td>
-        <td>&#8805;40.0</td>
-      </tr>
+      <tbody>
+        <tr>
+          <th>Category</th>
+          <th>BMI (kg/m2)</th>
+        </tr>
+        <tr>
+          <td>Underweight (Severe thinness)</td>
+          <td> &lt; 16.0</td>
+        </tr>
+        <tr>
+          <td>Underweight (Moderate thinness)</td>
+          <td>16.0 – 16.9</td>
+        </tr>
+        <tr>
+          <td>Underweight (Mild thinness)</td>
+          <td>17.0 – 18.4</td>
+        </tr>
+        <tr>
+          <td>Normal range</td>
+          <td>18.5 – 24.9</td>
+        </tr>
+        <tr>
+          <td>Overweight (Pre-obese)</td>
+          <td>25.0 – 29.9</td>
+        </tr>
+        <tr>
+          <td>Obese (Class I)</td>
+          <td>30.0 – 34.9</td>
+        </tr>
+        <tr>
+          <td>Obese (Class II)</td>
+          <td>35.0 – 39.9</td>
+        </tr>
+        <tr>
+          <td>Obese (Class III)</td>
+          <td>&#8805;40.0</td>
+        </tr>
+      </tbody>
     </table>
   );
 }
